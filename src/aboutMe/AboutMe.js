8c73acfd9dcf5daf045449bc6e9fe692a7f1e8cb
Prasad Anchawale <aprasad1011@gmail.com>
2021-10-04 10:47:08 +0530

    #2 updated fac icon and experience

--- src/aboutMe/AboutMe.js
+++ src/aboutMe/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <Section sectionTitle='About Me'>
             <div>
                 <p className="About-me-details">
-                    I'm a Full Stack Developer with 6 Years of experience in Web Application Developement.
+                    I'm a Full Stack Developer with 6.1 Years of experience in Web Application Developement.
                 </p>
                 <p className="About-me-details">
                     Currently, I'm working with Credit Suisse and responsible for developing new web applications / workflows 
@@ -16,6 +16,6 @@ const AboutMe = () => {
             </div>
         </Section>
     )
-}
+};
 
 export default AboutMe;
