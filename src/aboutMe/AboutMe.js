dfa8b988deabad348e3b024a60b4af5039b548f0
Prasad Anchawale <aprasad1011@gmail.com>
2022-09-22 19:54:05 +0530

    updated experience

--- src/aboutMe/AboutMe.js
+++ src/aboutMe/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <Section sectionTitle='About Me'>
             <div>
                 <p className="About-me-details">
-                    I'm a Full Stack Developer with 6.4 Years of experience in Web Application Developement.
+                    I'm a Full Stack Developer with 7.1 Years of experience in Web Application Developement.
                 </p>
                 <p className="About-me-details">
                     Currently, I'm working with Credit Suisse and responsible for developing new web applications / workflows 
